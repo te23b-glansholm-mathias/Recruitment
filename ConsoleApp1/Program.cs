4b697e64c2dfa2d43ec95f2de2e359a4eb505cd1
mathias glansholm <mathias.glansholm@elev.ga.ntig.se>
2024-10-16 14:56:00 +0200

    reqruitment

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,27 +1,26 @@
-﻿using System;
-using System.Security.Cryptography;
-
-Console.WriteLine("Hello welcome to yet another banger of a game");
+﻿Console.WriteLine("Hello welcome to yet another banger of a game");
 Console.WriteLine("Today you will reqruits different people to help you in battle");
 Console.WriteLine("Press enter to start");
+Console.ReadLine();
+Console.Clear();
 game();
 
-static void game()
+static void game() //everything is a function so I can call it
 {
     List<string> potentialReqruits = new List<string> { "Meaty michael", "frenchman", "royale George", "Dr. Rouge shooter", "september", "Nale", "Trues", "Zypheron", "Robert H. Smith" };
     List<string> aquiredReqruits = new List<string>();
 
     while (aquiredReqruits.Count < 3)
     {
-        for (int i = 0; i < potentialReqruits.Count; i++)
+        for (int i = 0; i < potentialReqruits.Count; i++) //writes out people you can reqruit 
         {
             Console.WriteLine($"{i + 1} - " + potentialReqruits[i]);
         }
 
         ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-        if (keyInfo.Key >= ConsoleKey.D1 && keyInfo.Key <= ConsoleKey.D9)
+        if (keyInfo.Key >= ConsoleKey.D1 && keyInfo.Key <= ConsoleKey.D9) //check so your input works
         {
-            int whichKey = (int)keyInfo.Key - (int)ConsoleKey.D1;
+            int whichKey = (int)keyInfo.Key - (int)ConsoleKey.D1; //Fixes your input to work with the list
 
             if (whichKey < potentialReqruits.Count)
             {
@@ -29,7 +28,7 @@ static void game()
                 aquiredReqruits.Add(potentialReqruits[whichKey]);
                 potentialReqruits.Remove(potentialReqruits[whichKey]);
                 Console.Clear();
-                Console.WriteLine($"you reqruited {aquiredReqruits[^1]}!");
+                Console.WriteLine($"you reqruited {aquiredReqruits[^1]}!"); //Writes the last index in the list
 
                 if (aquiredReqruits.Count < 3)
                 {
@@ -41,9 +40,9 @@ static void game()
                     Console.WriteLine("You can not reqruit anyone else.");
                     Console.WriteLine("\nyou have reqruited the following people:");
 
-                    for (int i = 0; i < aquiredReqruits.Count; i++)
+                    for (int i = 0; i < aquiredReqruits.Count; i++) //writes out the people you have reqruited
                     {
-                        Console.WriteLine(aquiredReqruits[i]);
+                        Console.WriteLine(aquiredReqruits[i]); 
                     }
 
                     Console.WriteLine("Press R to restart the game and backspace if you wanna remove a reqruit.");
@@ -89,7 +88,6 @@ static void game()
         {
             Console.WriteLine("\nPlease choose one of the reqruits");
         }
-
     }
 }
 
